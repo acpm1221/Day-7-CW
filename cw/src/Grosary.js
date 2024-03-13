2347a3e0c86a2aecb21e988ad5d53f4f997cae2a
Anmol Kumar <anmolcricket888@gmail.com>
2024-03-13 23:17:53 +0530

    project done

---
+++ cw/src/Grosary.js
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from "react";
+import "./Grosary.css";
+import Item from "./Item";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+function Grosary() {
+  const [items, setItems] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  useEffect(() => {
+    let result = localStorage.getItem("Groceryitems");
+    if (result) {
+      setItems(JSON.parse(result));
+    }
+  }, []);
+
+  
+  const addItem = () => {
+    if (inputValue.trim() === "") {
+      toast.error('Please enter a valid item.');
+      return;
+    }else{
+      const newData = {
+        Gitem: inputValue,
+      };
+  
+      const newDataUpdate = [...items, newData];
+      setItems(newDataUpdate);
+      localStorage.setItem("Groceryitems", JSON.stringify(newDataUpdate));
+      setInputValue("");
+      toast.success('Item added successfully.');
+    }
+
+  };
+  
+  return (
+    <>
+      <div className="mainDiv">
+        <br />
+        <h2>Grocery List</h2>
+        <div className="input">
+          <input
+            type="text"
+            className="groceryItem"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+          &nbsp;&nbsp;
+          <button
+            onClick={addItem}
+          >
+            Add Item
+          </button>
+        
+        </div>
+        {items.map((ele, index) => (
+          <Item
+            name={ele.Gitem}
+            key={index}
+            items={items}
+            idx={index}
+            setItems={setItems}
+          />
+        ))}
+      </div>
+      <ToastContainer
+        position="top-center"
+        
+      />
+    </>
+  );
+}
+
+export default Grosary;
---
+++ cw/src/Item.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import "./Item.css";
+
+import {  toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+
+function Item({ name, inputvalue, items, idx, setItems }) {
+  const [line, setline] = useState(false);
+
+  
+  const deleteHandler = (id) => {
+   
+    const data = [...items];
+    data.splice(idx, 1);
+    setItems(data);
+    localStorage.setItem("Groceryitems", JSON.stringify(data));
+   
+    toast.error("Item Deleted successfully!");
+    
+    
+  };
+
+  return (
+    <>
+      <div className="main_Container">
+        <div className="left">
+          <input
+            type="checkbox"
+            onChange={() => {
+              setline(true);
+            }}
+          />
+          <p style={{ textDecoration: line ? "line-through" : "none" }}>
+            {name}
+          </p>
+        </div>
+        <div className="right">
+          <button
+            onClick={() => {
+              deleteHandler()
+           
+            }}
+          >
+            Delete
+          </button>
+        </div>
+        
+      </div>
+    </>
+  );
+}
+
+export default Item;
